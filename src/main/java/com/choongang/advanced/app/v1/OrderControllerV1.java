181a3fcb5ff29b600b6de7f4dc3b4c129fe04c9d
seedevk8s <seedevk8s@gmail.com>
2025-05-17 12:56:54 +0900

    OrderControllerV1에 로그 추적기를 적용함

--- src/main/java/com/choongang/advanced/app/v1/OrderControllerV1.java
+++ src/main/java/com/choongang/advanced/app/v1/OrderControllerV1.java
@@ -1,5 +1,7 @@
 package com.choongang.advanced.app.v1;
 
+import com.choongang.advanced.trace.TraceStatus;
+import com.choongang.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderControllerV1 {
 
     private final OrderServiceV1 orderService;
+    private final HelloTraceV1 trace;
 
     /**
      * 주문 요청
@@ -17,7 +20,11 @@ public class OrderControllerV1 {
      */
     @GetMapping("/v1/request")
     public String request(String itemId) {
+
+        TraceStatus status = trace.begin("OrderControllerV1.request");
         orderService.orderItem(itemId);
+        trace.end(status);
+
         return "ok";
     }
 }
