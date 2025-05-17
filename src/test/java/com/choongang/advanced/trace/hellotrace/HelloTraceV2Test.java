9700f4c2bb27bfc4c4a624eae17ca6838def141f
seedevk8s <seedevk8s@gmail.com>
2025-05-17 20:12:17 +0900

    로그추적기 v2 생성

--- src/test/java/com/choongang/advanced/trace/hellotrace/HelloTraceV2Test.java
+++ src/test/java/com/choongang/advanced/trace/hellotrace/HelloTraceV2Test.java
@@ -3,19 +3,24 @@ package com.choongang.advanced.trace.hellotrace;
 import com.choongang.advanced.trace.TraceStatus;
 import org.junit.jupiter.api.Test;
 
-public class HelloTraceV1Test {
+public class HelloTraceV2Test {
 
      @Test
      void begin_end() {
-         HelloTraceV1 trace = new HelloTraceV1();
-         TraceStatus status = trace.begin("hello");
-         trace.end(status);
+         HelloTraceV2 trace = new HelloTraceV2();
+         TraceStatus status1 = trace.begin("hello1");
+         TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+
+         trace.end(status2);
+         trace.end(status1);
      }
 
     @Test
     void begin_exception() {
-        HelloTraceV1 trace = new HelloTraceV1();
-        TraceStatus status = trace.begin("hello");
-        trace.exception(status, new IllegalStateException());
+        HelloTraceV2 trace = new HelloTraceV2();
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(),"hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
     }
 }
