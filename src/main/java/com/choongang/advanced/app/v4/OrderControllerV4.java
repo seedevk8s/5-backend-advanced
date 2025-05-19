b58e1ce623da5434ab7dab7a4231a35efa63f48c
seedevk8s <seedevk8s@gmail.com>
2025-05-19 22:24:19 +0900

    v4에 기본내용 변경

---
+++ src/main/java/com/choongang/advanced/app/v4/OrderControllerV4.java
@@ -0,0 +1,41 @@
+package com.choongang.advanced.app.v4;
+
+import com.choongang.advanced.trace.TraceStatus;
+import com.choongang.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV4 {
+
+    private final OrderServiceV4 orderService;
+    private final LogTrace trace;       // LogTrace 인터페이스를 사용하여 로그 추적 기능을 구현
+
+    /**
+     * 주문 요청
+     * @param itemId 상품 아이디
+     * @return 결과
+     */
+    @GetMapping("/v4/request")
+    public String request(String itemId) {
+
+        TraceStatus status = null;
+        // 비즈니스 로직
+        // 주문 요청
+        try {
+            status = trace.begin("OrderControllerV4.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;  // 예외를 꼭 던져야 한다.
+        }
+
+
+
+
+    }
+}
---
+++ src/main/java/com/choongang/advanced/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,44 @@
+package com.choongang.advanced.app.v4;
+
+import com.choongang.advanced.trace.TraceStatus;
+import com.choongang.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepositoryV4.save()");
+
+            // 저장 로직
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            sleep(1000);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;  // 예외를 꼭 던져야 한다.
+        }
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
