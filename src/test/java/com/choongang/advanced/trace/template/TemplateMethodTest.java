c0011fe24f28784f5675f07511bb95e26bcb3c51
seedevk8s <seedevk8s@gmail.com>
2025-05-19 19:36:19 +0900

    변하는 부분과 변하지 않는 부분이 함께 있는 코드

---
+++ src/test/java/com/choongang/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,35 @@
+package com.choongang.advanced.trace.template;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0() {
+        // Client Code
+        logic1();
+        logic2();
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("비즈니스 로직2 종료, 시간={}", resultTime);
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        // 비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("비즈니스 로직1 종료, 시간={}", resultTime);
+    }
+}
