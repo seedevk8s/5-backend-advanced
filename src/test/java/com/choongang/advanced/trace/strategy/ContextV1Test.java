c44d8d60166fa5ee154624be5175918193521353
seedevk8s <seedevk8s@gmail.com>
2025-05-20 12:22:04 +0900

    전략 패턴 적용

--- src/test/java/com/choongang/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/com/choongang/advanced/trace/strategy/ContextV1Test.java
@@ -1,5 +1,8 @@
 package com.choongang.advanced.trace.strategy;
 
+import com.choongang.advanced.trace.strategy.code.strategy.ContextV1;
+import com.choongang.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import com.choongang.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import com.choongang.advanced.trace.template.code.AbstractTemplate;
 import com.choongang.advanced.trace.template.code.SubClassLogic1;
 import com.choongang.advanced.trace.template.code.SubClassLogic2;
@@ -36,7 +39,20 @@ public class ContextV1Test {
         log.info("비즈니스 로직1 종료, 시간={}", resultTime);
     }
 
+    /**
+     * 전략 패턴을 사용하여 비즈니스 로직을 분리합니다.
+     */
+    @Test
+    void strategyV1() {
+        // Client Code
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1); // 전략을 주입합니다.
+        context1.execute();
 
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2); // 전략을 주입합니다.
+        context2.execute();
+    }
 }
 
 
