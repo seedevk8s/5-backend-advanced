66e8434171b9b127b5bf355535d01e1b04f30bae
seedevk8s <seedevk8s@gmail.com>
2025-05-20 11:49:05 +0900

    해당인터페이스 구현체 추가

---
+++ src/test/java/com/choongang/advanced/trace/strategy/code/strategy/StrategyLogic2.java
@@ -0,0 +1,11 @@
+package com.choongang.advanced.trace.strategy.code.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StrategyLogic2 implements Strategy {
+    @Override
+    public void call() {
+        log.info("StrategyLogic2 call-비즈니스 로직1 실행");
+    }
+}
