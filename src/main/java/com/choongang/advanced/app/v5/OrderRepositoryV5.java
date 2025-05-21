02d54fcac0e71131274c94efba1e4325166fa745
seedevk8s <seedevk8s@gmail.com>
2025-05-21 17:57:12 +0900

    OrderRepositoryV5 구현

--- src/main/java/com/choongang/advanced/app/v5/OrderRepositoryV5.java
+++ src/main/java/com/choongang/advanced/app/v5/OrderRepositoryV5.java
@@ -18,6 +18,11 @@ public class OrderRepositoryV5 {
         this.traceTemplate = new TraceTemplate(trace); // 템플릿 객체 생성
     }
 
+    /**
+     * 주문 아이템 저장
+     * @param itemId 상품 아이디
+     * 템플릿 콜백 패턴의 동작(call())은 호출 코드(traceTemplate.execute())에 의해 트리거됩니다.
+     */
     public void save(String itemId) {
 
         traceTemplate.execute("OrderRepositoryV5.save()", new TraceCallback<Void>() {
