48c405a980973f36371dff5f74441a0afc141a18
seedevk8s <seedevk8s@gmail.com>
2025-05-21 17:08:35 +0900

    trace 의존관계 주입을 받으면서 필요한TraceTemplate 템플릿을 생성

--- src/main/java/com/choongang/advanced/app/v5/OrderControllerV5.java
+++ src/main/java/com/choongang/advanced/app/v5/OrderControllerV5.java
@@ -2,34 +2,37 @@ package com.choongang.advanced.app.v5;
 
 import com.choongang.advanced.trace.logtrace.LogTrace;
 import com.choongang.advanced.trace.template.AbstractTemplate;
+import com.choongang.advanced.trace.templatecallback.TraceCallback;
+import com.choongang.advanced.trace.templatecallback.TraceTemplate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 public class OrderControllerV5 {
 
     private final OrderServiceV5 orderService;
-    private final LogTrace trace;       // LogTrace 인터페이스를 사용하여 로그 추적 기능을 구현
+    private final TraceTemplate traceTemplate;
+
+    public OrderControllerV5(OrderServiceV5 orderService, LogTrace trace) {
+        this.orderService = orderService;
+        this.traceTemplate = new TraceTemplate(trace);
+    }
 
     /**
-     * 주문 요청
-     * @param itemId 상품 아이디
-     * @return 결과
+     *  * 주문 요청
+     *  * @param itemId 상품 아이디
+     *  * @return 주문 결과
+     *
      */
     @GetMapping("/v5/request")
     public String request(String itemId) {
-
-        // 템플릿 메서드 패턴을 사용하여 비즈니스 로직을 실행합니다.
-        AbstractTemplate<String> template = new AbstractTemplate<>(trace) {
+        return traceTemplate.execute("OrderControllerV5.request()", new TraceCallback<String>() {
             @Override
-            protected String call() {
+            public String call() {
                 orderService.orderItem(itemId);
                 return "ok";
             }
-        };
-        return template.execute("OrderControllerV4.request()");
-        // 기존의 코드와 동일한 기능을 수행합니다.
+        });
     }
 }
